8e8ca5504c4192fbc94b7557c0781d3cefd101cb
asven <asvennes@gmail.com>
2015-01-23 10:56:06 -0600

    adding files that -a didn't add

---
+++ Payroll/Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Common
+{
+    public static class Constants
+    {
+        public static double SocialSecurityPercentage = .062;
+        public static double MedicarePercentage = .0145;
+    }
+}
---
+++ Payroll/Common/FederalWithholdingTaxBracket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll.Common
+{
+    public class FederalWithholdingTaxBracket
+    {
+        public int FederalWithholdingTaxBracketID { get; set; }
+        public bool IsMarried { get; set; }
+        public double MoreThan { get; set; }
+        public double? LessThan { get; set; }
+        public double AmountToAdd { get; set; }
+        public double Percentage { get; set; }
+        public double ExcessOver { get; set; }
+    }
+}
